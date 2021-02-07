2df2f0318d4bfa3c3dabbaacb4acdbb2ec4ffda8
nelisriebezos <73901597+nelisriebezos@users.noreply.github.com>
2021-02-07 18:05:51 +0100

    practicum 2.1

---
+++ src/practicum2/Zwembad.java
@@ -0,0 +1,41 @@
+package practicum2;
+
+public class Zwembad {
+    private double breedte;
+    private double lengte;
+    private double diepte;
+
+    public Zwembad(double br, double le, double di) {
+        breedte = br;
+        lengte = le;
+        diepte = di;
+    }
+
+    public double getBreedte() {
+        return breedte;
+    }
+
+    public void setBreedte(double breedte) {
+        this.breedte = breedte;
+    }
+
+    public double getLengte() {
+        return lengte;
+    }
+
+    public void setLengte(double lengte) {
+        this.lengte = lengte;
+    }
+
+    public double getDiepte() {
+        return diepte;
+    }
+
+    public void setDiepte(double diepte) {
+        this.diepte = diepte;
+    }
+
+    public void berekenInhoud(){
+
+    }
+}
