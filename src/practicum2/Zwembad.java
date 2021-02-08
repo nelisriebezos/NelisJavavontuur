510f8978462c144baf7c0796120310409aff6123
nelisriebezos <73901597+nelisriebezos@users.noreply.github.com>
2021-02-08 14:34:54 +0100

    echt zwembad nu

--- src/practicum2/Zwembad.java
+++ src/practicum2/Zwembad.java
@@ -1,46 +1,51 @@
-package src.practicum2;
+package practicum2;
 
 public class Zwembad {
-    private double breedte;
-    private double lengte;
-    private double diepte;
-    private double inhoud;
+	private double breedte;
+	private double lengte;
+	private double diepte;
+	private double inhoud;
 
-    public Zwembad(double br, double le, double di) {
-        breedte = br;
-        lengte = le;
-        diepte = di;
-        berekenInhoud();
-    }
-    
-    public Zwembad() {	
-    }
+	public Zwembad(double br, double le, double di) {
+		breedte = br;
+		lengte = le;
+		diepte = di;
+	}
 
-    public double getBreedte() {
-        return breedte;
-    }
+	public Zwembad() {
+	}
 
-    public void setBreedte(double breedte) {
-        this.breedte = breedte;
-    }
+	public double getBreedte() {
+		return breedte;
+	}
 
-    public double getLengte() {
-        return lengte;
-    }
+	public void setBreedte(double breedte) {
+		this.breedte = breedte;
+	}
 
-    public void setLengte(double lengte) {
-        this.lengte = lengte;
-    }
+	public double getLengte() {
+		return lengte;
+	}
 
-    public double getDiepte() {
-        return diepte;
-    }
+	public void setLengte(double lengte) {
+		this.lengte = lengte;
+	}
 
-    public void setDiepte(double diepte) {
-        this.diepte = diepte;
-    }
+	public double getDiepte() {
+		return diepte;
+	}
 
-    public void berekenInhoud(){
-    	inhoud = this.breedte * this.diepte * this.lengte;
-    }
+	public void setDiepte(double diepte) {
+		this.diepte = diepte;
+	}
+
+	public double inhoud() {
+		return inhoud = breedte * lengte * diepte;
+	}
+
+	@Override
+	public String toString() {
+		return "Dit zwembad is " + breedte + " meter breed, " + lengte + " meter lang, en " + diepte + " meter diep";
+	}
+
 }
