81fd9f15fa74556f4c0996ffe231a72802bfd466
nelisriebezos <73901597+nelisriebezos@users.noreply.github.com>
2021-03-10 21:05:53 +0100

    push

--- src/Opdracht3InCode/Product.java
+++ src/Opdracht3InCode/Product.java
@@ -3,4 +3,12 @@ package Opdracht3InCode;
 public class Product {
 	private String omschrijving;
 
+	public Product(String omschrijving) {
+		super();
+		this.omschrijving = omschrijving;
+	}
+	
+	public String geefOmschrijving() {
+		return omschrijving;
+	}
 }
--- src/Opdracht3InCode/VerhuurProduct.java
+++ src/Opdracht3InCode/VerhuurProduct.java
@@ -1,24 +1,38 @@
 package Opdracht3InCode;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class VerhuurProduct extends Product {
 	private int verhuurPrijs;
 	private int borg;
-	private static ArrayList<VerhuurProduct> alleVerhuurProducten = new ArrayList<>();
-	private ArrayList<Exemplaar>exemplaren = new ArrayList<>();
+	private static List<VerhuurProduct> alleVerhuurProducten = new ArrayList<>();
+	private List<Exemplaar> exemplaren = new ArrayList<>();
 
-	public VerhuurProduct() {
+	static {
+		alleVerhuurProducten.add(new VerhuurProduct("a"));
+		alleVerhuurProducten.add(new VerhuurProduct("b"));
+		alleVerhuurProducten.add(new VerhuurProduct("c"));
 	}
-	
-	public String geefOmschrijving() {
-		
+
+	public VerhuurProduct(String omschrijving) {
+		super(omschrijving);
+
 	}
-	
+
+	public static List<String> geefAlle(x) {
+		List<String> result = new ArrayList<>();
+
+		for (VerhuurProduct p : alleVerhuurProducten) {
+			result.add(p.geefOmschrijving());
+		}
+		return result;
+	}
+
 	public void setExemplaren(Exemplaar exmp) {
 		exemplaren.add(exmp);
 	}
-	
+
 	public Exemplaar geefBeschikbareExemplaren(int serieNr) {
 		for (Exemplaar e : exemplaren) {
 			if (serieNr == e.getSerieNr())
